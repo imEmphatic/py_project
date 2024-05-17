1882106ceee62e2ab655dff52d4fcbe57abd6165
Aleksey Prusikov <imemphatic@yandex.ru>
2024-05-17 17:42:24 +0300

    add func subtraction number

--- funcs.py
+++ funcs.py
@@ -1,10 +1,22 @@
-def summ():
+def summ_numbers():
     print("Давайте сложим два числа\n")
     user_first_number = int(input("Введите первое число: "))
     user_second_number = int(input("Введите второе число: "))
 
-    print(f"\nРезультат: {user_first_number + user_second_number}")
+    print(f"\nРезультат: {user_first_number + user_second_number}\n")
 
 
 if __name__ == "__main__":
-    summ()
+    summ_numbers()
+
+
+def subtraction_numbers():
+    print("Давайте найдём разницу двух чисел\n")
+    user_first_number = int(input("Введите первое число: "))
+    user_second_number = int(input("Введите второе число: "))
+
+    print(f"\nРезультат: {user_first_number - user_second_number}\n")
+
+
+if __name__ == "__main__":
+    subtraction_numbers()
